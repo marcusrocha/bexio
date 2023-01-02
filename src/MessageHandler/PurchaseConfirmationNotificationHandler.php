a3455c11a90dcb860a50a51c8478ec62548223b0
Your Name <you@example.com>
2023-01-02 00:44:59 +0000

    Implement email sender

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -2,19 +2,25 @@
 namespace App\MessageHandler;
 
 use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mailer\MailerInterface;
 
 
 #[AsMessageHandler]
 class PurchaseConfirmationNotificationHandler {
+
+    public function __construct (private MailerInterface $mailer) {
+
+    }
 
     public function __invoke(PurchaseConfirmationNotification $notification)
     {
         // 1. Create a PDF contract note
         echo 'Creating a PDF contract note...<br>';
 
-        // 2. Email the contract note to the buyer
-        echo 'Emailing contract note to ' . $notification->getOrder()->getBuyer()->getEmail(). '<br>';
+        $email = (new Email())->from('marcus@bexio.com')->to($notification->getOrder()->getBuyer()->getEmail())->subject('proof of concept')->text('What do you think?');
+        $this->mailer->send($email);
     }
 
 }
